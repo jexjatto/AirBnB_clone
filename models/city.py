e8c62c41802f56f246f21a1bc46d24c612c30099
jexjatto <jextrim@gmail.com>
2023-02-21 16:10:17 +0100

    updated the authors.

---
+++ models/city.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+from models.base_model import BaseModel
+"""This module creates a User class"""
+
+
+class City(BaseModel):
+    """Class for managing city objects"""
+
+    state_id = ""
+    name = ""
